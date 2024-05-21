9c1c1a52340bb575f8aab4e50f9819f18fa96118
ethnical <ethnicalnightmare@gmail.com>
2024-05-21 15:19:39 +0200

    chore: rename the param

--- op-monitorism/global_events/cli.go
+++ op-monitorism/global_events/cli.go
@@ -10,24 +10,25 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+// args in CLI have to be standardized and clean.
 const (
-	L1NodeURLFlagName = "l1.node.url"
-	NicknameFlagName  = "nickname"
-	PathYamlFlagName  = "pathYaml"
+	L1NodeURLFlagName     = "l1.node.url"
+	NicknameFlagName      = "nickname"
+	PathYamlRulesFlagName = "PathYamlRules"
 )
 
 type CLIConfig struct {
-	L1NodeURL string
-	Nickname  string
-	pathYaml  string
+	L1NodeURL     string
+	Nickname      string
+	PathYamlRules string
 	// Optional
 }
 
 func ReadCLIFlags(ctx *cli.Context) (CLIConfig, error) {
 	cfg := CLIConfig{
-		L1NodeURL: ctx.String(L1NodeURLFlagName),
-		Nickname:  ctx.String(NicknameFlagName),
-		pathYaml:  ctx.String(PathYamlFlagName),
+		L1NodeURL:     ctx.String(L1NodeURLFlagName),
+		Nickname:      ctx.String(NicknameFlagName),
+		PathYamlRules: ctx.String(PathYamlRulesFlagName),
 	}
 
 	return cfg, nil
@@ -48,7 +49,7 @@ func CLIFlags(envVar string) []cli.Flag {
 			Required: true,
 		},
 		&cli.StringFlag{
-			Name:     PathYamlFlagName,
+			Name:     PathYamlRulesFlagName,
 			Usage:    "Path to the yaml file containing the events to monitor",
 			EnvVars:  opservice.PrefixEnvVar(envVar, "PATH_YAML"), //need to change the name to BLOCKCHAIN_NAME
 			Required: true,
