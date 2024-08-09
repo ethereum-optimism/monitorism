c6ec8c368ac1fd6471c4f44349a8519b3f494a30
ethnical <ethnicalnightmare@gmail.com>
2024-08-09 10:54:20 +0200

    feat: add the data logic and also a failsafe with (yes/no) to avoid undesired pause.

--- op-monitorism/psp_executor/monitor.go
+++ op-monitorism/psp_executor/monitor.go
@@ -3,18 +3,17 @@ package psp_executor
 import (
 	"context"
 	"crypto/ecdsa"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"github.com/ethereum-optimism/optimism/op-service/client"
 	"github.com/ethereum-optimism/optimism/op-service/metrics"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/prometheus/client_golang/prometheus"
 	"math/big"
 )
@@ -43,11 +42,24 @@ type Monitor struct {
 func NewAPI(ctx context.Context, log log.Logger, m metrics.Factory, cfg CLIConfig) (*Monitor, error) {
 	log.Info("Creating the API psp_executor.")
 	client, err := ethclient.Dial(cfg.NodeUrl)
+	if cfg.NodeUrl != "http://localhost:8545" {
+		log.Warn("This is not the RPC localhost are you sure you want to continue (yes/no)")
+		var response string
+		fmt.Scanln(&response)
+		if response != "yes" {
+			log.Crit("Not yes, We Exiting the program.")
+		}
+	}
 	if err != nil {
 		log.Crit("Failed to connect to the Ethereum client: %v", err.Error())
 	}
+	data, err := hex.DecodeString(cfg.hexString)
+	if err != nil {
+		fmt.Println("Error decoding hex string:", err)
+		return &Monitor{}, err
+	}
 
-	txHash, err := sendTransaction(client, cfg.privatekeyflag, cfg.ReceiverAddressFlagName, big.NewInt(1)) // 1 wei
+	txHash, err := sendTransaction(client, cfg.privatekeyflag, cfg.ReceiverAddressFlagName, big.NewInt(1), data) // 1 wei
 	if err != nil {
 		log.Crit("Failed to send transaction: %v", err)
 	}
@@ -69,33 +81,6 @@ func PSPExecutionPause(request_operator string, request_timestamp string) {
 }
 
 func (m *Monitor) Run(ctx context.Context) {
-	m.log.Info("querying balances...")
-	batchElems := make([]rpc.BatchElem, len(m.accounts))
-	for i := 0; i < len(m.accounts); i++ {
-		batchElems[i] = rpc.BatchElem{
-			Method: "eth_getBalance",
-			Args:   []interface{}{m.accounts[i].Address, "latest"},
-			Result: new(hexutil.Big),
-		}
-	}
-	if err := m.rpc.BatchCallContext(ctx, batchElems); err != nil {
-		m.log.Error("failed getBalance batch request", "err", err)
-		m.unexpectedRpcErrors.WithLabelValues("balances", "batched_getBalance").Inc()
-		return
-	}
-
-	for i := 0; i < len(m.accounts); i++ {
-		account := m.accounts[i]
-		if batchElems[i].Error != nil {
-			m.log.Error("failed to query account balance", "address", account.Address, "nickname", account.Nickname, "err", batchElems[i].Error)
-			m.unexpectedRpcErrors.WithLabelValues("balances", "getBalance").Inc()
-			continue
-		}
-
-		ethBalance := weiToEther((batchElems[i].Result).(*hexutil.Big).ToInt())
-		m.balances.WithLabelValues(account.Address.String(), account.Nickname).Set(ethBalance)
-		m.log.Info("set balance", "address", account.Address, "nickname", account.Nickname, "balance", ethBalance)
-	}
 }
 
 func (m *Monitor) Close(_ context.Context) error {
@@ -103,8 +88,9 @@ func (m *Monitor) Close(_ context.Context) error {
 	return nil
 }
 
-func sendTransaction(client *ethclient.Client, privateKeyStr string, toAddressStr string, amount *big.Int) (string, error) {
+func sendTransaction(client *ethclient.Client, privateKeyStr string, toAddressStr string, amount *big.Int, data []byte) (string, error) {
 	// Convert the private key string to a private key type
+	// TODO: Need to check if there is the `0x` if yes remove it from the string.
 	privateKey, err := crypto.HexToECDSA(privateKeyStr)
 	if err != nil {
 		return "", fmt.Errorf("invalid private key: %v", err)
@@ -133,17 +119,13 @@ func sendTransaction(client *ethclient.Client, privateKeyStr string, toAddressSt
 	}
 
 	// Set up the transaction parameters
-	value := amount           // Amount of Ether to send
-	gasLimit := uint64(21000) // In units
+	value := amount                // Amount of Ether to send
+	gasLimit := uint64(10 * 21000) // In units
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return "", fmt.Errorf("failed to suggest gas price: %v", err)
 	}
 
-	// Create the transaction
-	data := []byte{}
-	// data = append(data, []byte("Hello, World!")...)
-	data = []byte("H")
 	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
 
 	// Sign the transaction with the private key
@@ -163,9 +145,6 @@ func sendTransaction(client *ethclient.Client, privateKeyStr string, toAddressSt
 	}
 
 	return signedTx.Hash().Hex(), nil
-}
-
-func main() {
 }
 
 func weiToEther(wei *big.Int) float64 {
