8f7369e46704c3555877816673c564fe532474aa
ethnical <ethnicalnightmare@gmail.com>
2024-09-26 10:13:07 +0200

    chore: nits

--- op-defender/psp_executor/utils.go
+++ op-defender/psp_executor/utils.go
@@ -7,7 +7,7 @@ import (
 
 const ether = 1e18
 
-// weiToEther converts a wei value to ether return a float64 return an error if the float is toolarge
+// weiToEther converts a wei value to ether return a float64 return an error if the float is too large.
 func weiToEther(wei *big.Int) (float64, error) {
 	num := new(big.Rat).SetInt(wei)
 	denom := big.NewRat(ether, 1)
