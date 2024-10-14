de4401fd6ea39abd8d02e232765eae5ee1f53f51
raffaele-oplabs <raffaele@oplabs.co>
2024-10-14 12:39:37 +0200

    check for nil

--- op-monitorism/faultproof_withdrawals/monitor.go
+++ op-monitorism/faultproof_withdrawals/monitor.go
@@ -282,6 +282,10 @@ func (m *Monitor) Run(ctx context.Context) {
 // It returns any events detected during the consumption that requires to be re-analysed again at a later stage (when the event referenced DisputeGame completes).
 func (m *Monitor) ConsumeEvents(enrichedWithdrawalEvents map[common.Hash]*validator.EnrichedProvenWithdrawalEvent) error {
 	for _, enrichedWithdrawalEvent := range enrichedWithdrawalEvents {
+		if enrichedWithdrawalEvent == nil {
+			m.log.Error("WITHDRAWAL: enrichedWithdrawalEvent is nil in ConsumeEvents")
+			panic("WITHDRAWAL: enrichedWithdrawalEvent is nil in ConsumeEvents")
+		}
 		m.log.Info("processing withdrawal event", "event", enrichedWithdrawalEvent)
 		err := m.withdrawalValidator.UpdateEnrichedWithdrawalEvent(enrichedWithdrawalEvent)
 		//upgrade state to the latest L2 height	after the event is processed
