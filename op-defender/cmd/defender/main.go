8578a8b8a43aff7fb52d3d24ce3faeea1e457dba
crStiv <cryptostiv7@gmail.com>
2024-12-15 21:57:46 +0100

    fix: multiple typos of different importance (#102)
    * article redundancy
    
    * preposition redundancy
    
    * typo
    
    * typo
    
    * multiple typos
    
    * multiple typos
    
    * typo

--- op-defender/cmd/defender/main.go
+++ op-defender/cmd/defender/main.go
@@ -19,7 +19,7 @@ func main() {
 	oplog.SetupDefaults()
 	app := newCli(GitCommit, GitDate)
 
-	// sub-commands set up their individual interrupt lifecycles, which can block on the given interrupt as needed.
+	// sub-commands set up their individual interrupt lifecycles, which can block the given interrupt as needed.
 	ctx := opio.WithInterruptBlocker(context.Background())
 	if err := app.RunContext(ctx, os.Args); err != nil {
 		log.Error("application failed", "err", err)
